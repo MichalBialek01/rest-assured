9d88807f4492343229a3213ba513bc03ab7465e2
Michał Białek <kontakt.michalbialek@gmail.com>
2024-09-27 13:25:23 +0200

    Initial Commit

---
+++ src/main/java/com/example/restassuredtests/RestAssuredTestsApplication.java
@@ -0,0 +1,13 @@
+package com.example.restassuredtests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestAssuredTestsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestAssuredTestsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/restassuredtests/RestAssuredTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restassuredtests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestAssuredTestsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
